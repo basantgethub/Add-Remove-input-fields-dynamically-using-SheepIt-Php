d1825954ab4ad62fcceeba0528ebcb3c0f4fab15
Basant kumar <basantgithub@gmail.com>
2016-09-07 07:28:00 -0400

    Update controllers.php

--- CI-useing-code/controllers.php
+++ CI-useing-code/controllers.php
@@ -1,53 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-define('ROOT_PATH', dirname(__FILE__));
-include_once(ROOT_PATH.'/brand_base.php');
-
-class Customize_template extends Brand_base {
+class controllers extends  controllers{
 
 	function __construct () {
 		parent::__construct(); 
 	}
 	
 	 function displayIcon(){
-                if(!empty($_REQUEST['submit']) && !empty($this->notificationID)){
-                    //print_r($_REQUEST['advselection']);die;
-                        if(!empty($_REQUEST['type'])){
-                                $params['type'] = $_REQUEST['type'];
-                        }
-                        if(isset($_REQUEST['crouselselection'])){
-                                $crouselselection = array();
-                                $c=0;
-                                $length = count($_REQUEST['crouselselection']);
-                                for($i=0; $i < $length; $i+=3){
-                                    if(!empty($_REQUEST['crouselselection'][$i]['value'])){
-                                        $crouselselection[$c]['d'] = $_REQUEST['crouselselection'][$i]['value'];
-                                        $crouselselection[$c]['k'] = $_REQUEST['crouselselection'][$i+1]['value'];
-                                        $crouselselection[$c]['v'] = $_REQUEST['crouselselection'][$i+2]['value'];
-                                        $c++;
-                                    }
-                                }
-                                
-                                $params['crousel'] = serialize($crouselselection);
-                                
+                if(!empty($_REQUEST['submit'])){
+                if(isset($_REQUEST['crouselselection'])){
+                        $crouselselection = array();
+                        $c=0;
+                        $length = count($_REQUEST['crouselselection']);
+                        for($i=0; $i < $length; $i+=3){
+                            if(!empty($_REQUEST['crouselselection'][$i]['value'])){
+                                $crouselselection[$c]['d'] = $_REQUEST['crouselselection'][$i]['value'];
+                                $crouselselection[$c]['k'] = $_REQUEST['crouselselection'][$i+1]['value'];
+                                $crouselselection[$c]['v'] = $_REQUEST['crouselselection'][$i+2]['value'];
+                                $c++;
+                            }
                         }
                         
-                        //print_r($params); die;
-                        $result = $this->m_notifications->update($this->notificationID, $this->selfUID, $params);
-                        //print_r($result); 
-                        if($result > 0){
-                                $data['responseType'] = 'success';
-                                $data['message'] = 'Successfully updated';
-                        } else {
-                                $data = array( 'message' => 'Please try again!');			
-                        }
-			$this->_sendResponse($data);
-		}
-                $data = array();
-                $data['crouselselection'] = unserialize($this->notificationInfo['crousel']);
-              
-                $response['html'] = $this->_loadView('retailer/customize/view',$data,true);
-		$this->_sendResponse($response);
+                        $params['crousel'] = serialize($crouselselection);
+                        
+                }
+                
+                $data['cyz'] = unserialize('$restult');
         }
 }	
 	
